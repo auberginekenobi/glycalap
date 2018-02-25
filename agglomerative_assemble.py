6cef73691460e0751fb47dd085bbe7835bd3592f
Owen Chapman <osc12013@mymail.pomona.edu>
2018-02-25 13:29:19 -0800

    Doc for agglomerative_assemble

---
+++ agglomerative_assemble.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+'''
+function agglomerative_assemble
+author Owen Chapman auberginekenobi
+2/25/2018
+'''
+
+def agglomerative_assemble(glycan_list):
+	'''
+	Assembles contigs agglomeratively from a list of glycans. Objective function
+	is to minimize the number of nodes in the resulting contig set, tiebroken
+	by minimizing the number of glycans in the resulting contig set.
+	Input: 
+		glycan_list - list of Glycan objects
+	Output:
+		contigs - list of Glycan objects
+	'''
+	pass
