1cc9e919f69607da45ebf1a10dd2a0bd027513ba
Owen Chapman <osc12013@mymail.pomona.edu>
2018-02-26 15:14:24 -0800

    alpha version of agglomerative alg. not tested.

--- agglomerative_assemble.py
+++ agglomerative_assemble.py
@@ -5,36 +5,73 @@ author Owen Chapman auberginekenobi
 2/25/2018
 '''
 
-def agglomerative_assemble(glycan_list):
-	'''
-	Assembles contigs agglomeratively from a list of glycans. Objective function
-	is to minimize the number of nodes in the resulting contig set, tiebroken
-	by minimizing the number of glycans in the resulting contig set.
-	Input: 
-		glycan_list - list of Glycan objects
-	Output:
-		contigs - list of Glycan objects
-	'''
-	contigs = glycan_list
-	# find pairwise overlap scores across all glycan pairs
-	distance_matrix = {} # distance : [(Glycan, Glycan)]
-	for i in range(len(glycan_list)):
-		gi = glycan_list[i]
-		for j in range(i,len(glycan_list)):
-			gj = glycan_list[j]
-			print(i,j)
-			d,_ = overlap_score(gi,gj)
-			if d in distance_matrix:
-				distance_matrix[d].append((gi,gj))
-			else:
-				distance_matrix[d] = [(gi,gj)]
+from overlap_combine import *
+from math import inf
 
-	# while there exists a nonzero overlap score:
-	max_score = max(distance_matrix.keys())
-	while max_score>0:
-		# Find the two glycans with the highest overlap
-		# (arbitrary selection)
-		g1, g2 = distance_matrix[max_score].pop()
-		# Combine them
-		# update datastructures of distances and contigs
-	return contigs
+def find_max(adjacency_dict):
+    '''
+    Finds the maximum element in an adjacency dictionary
+    adjacency_dict must be of the form
+        obj: [(int, obj, obj)]
+    '''
+    max_val = -inf
+    (key, index) = (None,None)
+    for k in adjacency_dict:
+        for i in range(len(adjacency_dict[k])):
+            val = adjacency_dict[k][i][0]
+            if val > max_val:
+                max_val = val
+                (key,index) = (k,i)
+    return (max_val, key, index)
+
+
+def agglomerative_assemble(glycan_list):
+    '''
+    Assembles contigs agglomeratively from a list of glycans. Objective function
+    is to minimize the number of nodes in the resulting contig set, tiebroken
+    by minimizing the number of glycans in the resulting contig set.
+    Input: 
+        glycan_list - list of Glycan objects
+    Output:
+        contigs - list of Glycan objects
+    '''
+    # find pairwise overlap scores across all glycan pairs
+    distance_matrix = {} # Glycan : [(d, Glycan,(Glycan,Glycan))]
+    for i in range(len(glycan_list)):
+        gi = glycan_list[i]
+        for j in range(i+1,len(glycan_list)):
+            gj = glycan_list[j]
+            d,o = overlap_score(gi,gj)
+            o = o[0] if len(o)>0  else []
+            if d in distance_matrix:
+                distance_matrix[gi].append((d,gj,o))
+            else:
+                distance_matrix[gi] = [(d,gj,o)]
+
+    # while there exists a nonzero overlap score:
+    max_score, key, index = find_max(distance_matrix)
+    while max_score>0:
+        # Find the two glycans with the highest overlap
+        # (arbitrary selection)
+        g1 = key
+        g2 = distance_matrix[key][index][1]
+        o = distance_matrix[key][index][2]
+        # Combine them
+        g3 = overlap_combine(g1,g2,o)
+        # update datastructure of distances 
+        if g1 in distance_matrix:
+            distance_matrix.pop(g1)
+        if g2 in distance_matrix:
+            distance_matrix.pop(g2)
+        for key,value in distance_matrix.items():
+            if g1 in value or g2 in value:
+                distance_matrix[key].remove(value)
+        distance_matrix[g3] = []
+        for key in distance_matrix.keys():
+            if key == g3:
+                continue
+            d,o = overlap_score(g3,key)
+            o = o[0] if len(o)>0  else []
+            distance_matrix[g3].append((d,key,o))
+        max_score, key, index = find_max(distance_matrix)
+    return list(distance_matrix.keys())
--- agglomerative_sandbox.py
+++ agglomerative_sandbox.py
@@ -7,22 +7,15 @@ author Owen Chapman auberginekenobi
 '''
 
 from GlycanParser import *
-from GlycanFragmentation import *
 from overlap_combine import *
 from agglomerative_assemble import *
 
 
-VIM="alpha-D-NeupAc-(2->3)-beta-D-Galp-(1->4)-beta-D-GlcpNAc(1->"
-a=Glycan(VIM)
-b=Glycan(VIM)
-c=Glycan(VIM)
+sialyl_lewis_a = "alpha-D-NeupAc-(2->3)-beta-D-Galp-(1->3)[alpha-L-Fucp-(1->4)]-beta-D-GlcpNAc(1->"
+lewis_a = "beta-D-Galp-(1->3)[alpha-L-Fucp-(1->4)]-?-D-GlcpNAc(1->"
+notVIM = "alpha-D-NeupAc-(2->3)-beta-D-Galp-(1->3)-beta-D-GlcpNAc(1->"
+a=Glycan(sialyl_lewis_a)
+b=Glycan(lewis_a)
+c=Glycan(notVIM)
 l = [a,b,c]
-score, alns = overlap_score(a,b)
-print(score)
-print("%%%%%")
-print(alns[0])
-print(alns[1])
-print('%%%%%')
-print(a)
-overlap_combine(a,b,alns[3])
-print(a==b)
+print(agglomerative_assemble(l))
