49d2aacf4596f2836cb2799d55415565dca5ab1c
jtsorren <jtsorren@eng.ucsd.edu>
2018-02-26 16:17:25 -0800

    bugfix: fixed the test methods

--- AgglomerativeTestSuite.py
+++ AgglomerativeTestSuite.py
@@ -1,9 +1,9 @@
 # jtsorren
 
 from GlycanParser import *
-# from OwensBeautifulAgglomerativeAlgorithm import *
+from agglomerative_assemble import *
 
-def Test(name, expecteds, observed):
+def Test(name, expecteds, observeds):
     '''
     Test suite for the Agglomerative algorithm. 
     This function loops through possible accepted 
@@ -11,20 +11,25 @@ def Test(name, expecteds, observed):
     '''
     failed = 0
     if len(expecteds) == 0:
-        if expecteds == observed:
+        if Glycan.equals(expecteds, observeds):
             print('Passed test {}, yay!'.format(name))
+            print()
         else:
             print('Failed test {}. expected {} got {}, boo!'.format(name, expecteds, observed))
+            print()
     else:
         for expected in expecteds:
-            try:
-                assert(expected == observed)
-            except:
-                failed += 1
-        if failed == len(expecteds):
-            print('Failed test {}. expected {} got {}, boo!'.format(name, expecteds, observed))
-        else:
-            print('Passed test {}, yay!'.format(name))
+            for observed in observeds:
+                try:
+                    assert(Glycan.equals(expected, observed))
+                except:
+                    failed += 1
+            if failed == len(expecteds):
+                print('Failed test {}. expected {} got {}, boo!'.format(name, expecteds, observed))
+                print()
+            else:
+                print('Passed test {}, yay!'.format(name))
+                print()
         
 def TestOne():
     '''
@@ -32,7 +37,7 @@ def TestOne():
     '''
     fragments = []
     expecteds = []
-    Test(1, expecteds, OwensBeautifulAgglomerativeAlgorithm(fragments))
+    Test(1, expecteds, agglomerative_assemble(fragments))
     return
 
 def TestTwo():
@@ -45,7 +50,7 @@ def TestTwo():
                   Glycan(['L-Fucp', 'D-GlcpNAc'], [{1: ('alpha', 1, 3)}, {0: ('alpha', 3, 1)}])],
                  [Glycan(['L-Fucp', 'D-GlcpNAc'], [{1: ('alpha', 1, 3)}, {0: ('alpha', 3, 1)}]), 
                   Glycan(['L-Fucp', 'D-Galp'], [{1: ('alpha', 1, 2)}, {0: ('alpha', 2, 1)}])]]
-    Test(2, expecteds, OwensBeautifulAgglomerativeAlgorithm(fragments))
+    Test(2, expecteds, agglomerative_assemble(fragments))
     return
 
 def TestThree():
@@ -55,7 +60,7 @@ def TestThree():
     fragments = [Glycan(['L-Fucp', 'D-Galp'], [{1: ('alpha', 1, 2)}, {0: ('alpha', 2, 1)}]), 
                  Glycan(['L-Fucp', 'D-Galp'], [{1: ('alpha', 1, 2)}, {0: ('alpha', 2, 1)}])]
     expecteds = [Glycan(['L-Fucp', 'D-Galp'], [{1: ('alpha', 1, 2)}, {0: ('alpha', 2, 1)}])]
-    Test(3, expecteds, OwensBeautifulAgglomerativeAlgorithm(fragments))
+    Test(3, expecteds, agglomerative_assemble(fragments))
     return
 
 def TestFour():
@@ -67,7 +72,7 @@ def TestFour():
                  Glycan(['D-GlcpNAc', 'D-GlcpNAc', 'D-GlcpNAc'], [{1: ('beta', 1, 4)}, {0: ('beta', 4, 1), 2: ('beta', 1, 4)}, {1: ('beta', 4, 1)}]), 
                  Glycan(['D-Manp', 'D-GlcpNAc'], [{1: ('alpha', 4, 1)}, {0: ('alpha', 1, 4)}])]
     expecteds = [Glycan(['L-Fucp', 'D-GlcpNAc', 'D-GlcpNAc', 'D-GlcpNAc', 'D-Manp'], [{1: ('alpha', 1, 4)}, {0: ('alpha', 4, 1), 2: ('beta', 1, 4)}, {1: ('beta', 4, 1), 3: ('beta', 1, 4)}, {2: ('beta', 4, 1), 4: ('alpha', 1, 4)}, {3: ('alpha', 4, 1)}])]
-    Test(4, expecteds, OwensBeautifulAgglomerativeAlgorithm(fragments))
+    Test(4, expecteds, agglomerative_assemble(fragments))
     return
 
 def TestFive():
@@ -78,11 +83,11 @@ def TestFive():
                  Glycan(['L-Fucp', 'D-GlcpNAc', 'D-Manp'], [{1: ('alpha', 3, 1)}, {0: ('alpha', 1, 3), 2: ('beta', 4, 1)}, {1: ('beta', 1, 4)}]), 
                  Glycan(['L-Fucp', 'D-Manp'], [{1: ('beta', 1, 1)}, {0: ('beta', 1, 1)}])]
     expecteds = [Glycan(['L-Fucp', 'D-GlcpNAc', 'L-Fucp', 'D-Manp', 'D-Manp'], [{1: ('alpha', 1, 3)}, {0: ('alpha', 3, 1), 2: ('alpha', 1, 3), 4: ('beta', 4, 1)}, {1: ('alpha', 3, 1), 3: ('beta', 1, 1)}, {2: ('beta', 1, 1)}, {1: ('beta', 1, 4)}])]
-    Test(5, expecteds, OwensBeautifulAgglomerativeAlgorithm(fragments))
+    Test(5, expecteds, agglomerative_assemble(fragments))
     return
 
-#TestOne()
-#TestTwo()
-#TestThree()
-#TestFour()
-#TestFive()
+TestOne()
+TestTwo()
+TestThree()
+TestFour()
+TestFive()
